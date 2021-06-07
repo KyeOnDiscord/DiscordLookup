e7c580c87365abb2a6611ec66fe79a39691cc2cd
Kye <36981621+kyeondiscord@users.noreply.github.com>
2021-06-07 19:55:49 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace DiscordLookup
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(105, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 32);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Discord User ID:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(200)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(105, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 82);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lookup Discord User";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(459, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 195);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 124);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(200)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(758, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(200)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(722, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(200)))));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(232, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(348, 82);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Get raw user info";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(81)))), ((int)(((byte)(181)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Discord Lookup";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using Json.Net;
+using System.Diagnostics;
+using System.IO;
+namespace DiscordLookup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region FormMoveable
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+        #endregion
+        #region Tokens
+        //Discord for some reason needs a bot token to get a user, here are just some bot tokens that don't do anything, lmao try "nuking" somethig lmfao. I put 3 tokens so each token can have different rate limits just incase and stuff like that.
+        public readonly string[] tokens = { "ODUxMDM3NTY5NDQ4ODA0MzUy.YLycgA.tyY8oNsmrdfHI_pREqABsThbaC8", "ODUxMDQ1NzkxNDAxMTE1NjU4.YLykKA._caRDXKcb9nDY_aNZ76mjS6_WPU", "ODUxMDQ1OTE5Njk3MDEwNjg4.YLykRg.s3DoxGt2SCzganeOIjVfnzGdOfc" };
+        #endregion
+        private void button1_Click(object sender, EventArgs e) => GetDiscordUser(textBox1.Text);
+        private void pictureBox1_Click(object sender, EventArgs e) => GetDiscordUser(Clipboard.GetText());
+        private void button2_Click(object sender, EventArgs e) => Process.GetCurrentProcess().Kill();
+        private void button3_Click(object sender, EventArgs e) => WindowState = FormWindowState.Minimized;
+        private void GetDiscordUser(string UserID)
+        {
+            //TryParse is just a shorter way of a try catch
+            if (ulong.TryParse(UserID, out _))
+            {
+                textBox1.Text = UserID;
+                using (WebClient web = new WebClient())
+                {
+                    web.Headers.Add(HttpRequestHeader.Authorization, "Bot " + tokens[new Random().Next(0, tokens.Length)]);
+                    string data = web.DownloadString($"https://discord.com/api/v8/users/{textBox1.Text}");
+                    usr = JsonNet.Deserialize<DiscordUser>(data);
+                    usr.avatar = $"https://cdn.discordapp.com/avatars/{usr.id}/{usr.avatar}";
+                    usr.ConvertedFlags = CalculateUserFlags(usr.public_flags);
+                    pictureBox2.ImageLocation = usr.avatar;
+                    label2.Text = $"ID: {usr.id}\nUsername: {usr.username}#{usr.discriminator}\nUser Flags: {usr.ConvertedFlags}";
+                }
+            }
+            else
+            {
+                textBox1.Clear();
+                MessageBox.Show("That isn't a valid Discord User ID!", "DiscordLookup", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+        DiscordUser usr = new DiscordUser();
+        public class DiscordUser
+        {
+            public string id { get; set; }
+            public string username { get; set; }
+            public string avatar { get; set; }
+            public string discriminator { get; set; }
+            public int public_flags { get; set; }
+            public string ConvertedFlags { get; set; }//Self defined, not defined by discord
+        }
+
+        private string CalculateUserFlags(int number)
+        {
+            //https://discord.com/developers/docs/resources/user#user-object-user-flags
+            switch (number)
+            {
+                //case 0: return "None";     //Default case will handle this
+                case 1 << 0: return "Discord Employee";
+                case 1 << 1: return "Partnered Server Owner";
+                case 1 << 2: return "HypeSquad Events";   
+                case 1 << 3: return  "Bug Hunter Level 1"; 
+                case 1 << 6: return  "House Bravery"; 
+                case 1 << 7: return  "House Brilliance";
+                case 1 << 8: return  "House Balance";  
+                case 1 << 9: return  "Early Supporter";
+                case 1 << 10: return  "Team User";
+                case 1 << 14: return  "Bug Hunter Level 2";
+                case 1 << 16: return  "Verified Bot"; 
+                case 1 << 17: return  "Early Verified Bot Developer";  
+                case 1 << 18: return  "Discord Certified Moderator";
+                default: return "None";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (usr.id != null)
+            {
+                string filename = usr.id + "_export.txt";
+                File.WriteAllText(filename, $"UserID={usr.id}\nUsername={usr.username}\nDiscriminator={usr.discriminator}\nAvatar={usr.avatar}\nPublicFlags={usr.public_flags}\nUserFlags={usr.ConvertedFlags}");
+                Process.Start($"notepad.exe", $"{Path.GetFullPath(filename)}");
+            }
+            else
+                MessageBox.Show("That isn't a valid Discord User ID!", "DiscordLookup", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
